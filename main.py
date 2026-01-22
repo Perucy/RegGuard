f7987394d0e2def18cf8d5d22fc033ee3ae6487b
Perucy <perucymussiba@gmail.com>
2026-01-22 12:55:53 -0500

    server runner

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from regguard!")
+import uvicorn
+from src.regguard.core.config import settings
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(
+        "regguard.api.main:app",
+        host=settings.HOST,
+        port=settings.PORT,
+        log_level="info",
+        reload=settings.ENVIRONMENT == "development"
+    )
