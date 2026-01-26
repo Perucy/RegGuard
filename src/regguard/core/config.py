71abe0dcb0c342acdda8ccbe4c4b20aced013a02
Perucy <perucymussiba@gmail.com>
2026-01-26 16:12:27 -0500

    added model config and load dotenv to ensure that we do not need to use "export" to export variables everytime when running langsmith

--- src/regguard/core/config.py
+++ src/regguard/core/config.py
@@ -1,8 +1,18 @@
 """ Config management using environment variable """
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Settings(BaseSettings):
     """ App settings loaded from env variables """
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        extra="ignore",
+    )
 
     # Application settings
     ENVIRONMENT: str = "development"
@@ -10,12 +20,13 @@ class Settings(BaseSettings):
 
     HOST: str = "0.0.0.0"
     PORT: int = 8000
-    
+
     # Api keys
     ANTHROPIC_API_KEY: str
 
-    class Config:
-        env_file = ".env"
-        case_sensitive = False
+    # LANGSMITH
+    LANGSMITH_API_KEY: str | None = None
+    LANGSMITH_TRACING: bool = False
+    LANGSMITH_PROJECT: str = "regguard-dev"
 
 settings = Settings()
